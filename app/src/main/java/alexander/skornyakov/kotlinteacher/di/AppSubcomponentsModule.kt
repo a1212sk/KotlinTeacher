0b18cac9335dbf40ccfa8900bf58968b2c6093cb
Alexander Skornyakov <a1212sk@gmail.com>
2020-06-05 15:53:02 +0300

    make firebaseauth a singleton

--- app/src/main/java/alexander/skornyakov/kotlinteacher/di/AppSubcomponentsModule.kt
+++ app/src/main/java/alexander/skornyakov/kotlinteacher/di/AppSubcomponentsModule.kt
@@ -2,7 +2,18 @@ package alexander.skornyakov.kotlinteacher.di
 
 import alexander.skornyakov.kotlinteacher.di.main.MainComponent
 import alexander.skornyakov.kotlinteacher.di.start.StartComponent
+import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module(subcomponents = [StartComponent::class, MainComponent::class])
-abstract class AppSubcomponentsModule
+class AppSubcomponentsModule{
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        @Singleton
+        fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+    }
+}
